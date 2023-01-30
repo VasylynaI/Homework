9f16a71bf92d3a40d2cca56cbe0a79273d5eb2ed
Vasylyna Ivanyshak <vasylynaivanyshak@MacBook-Air-Vasylyna.local>
2023-01-30 17:23:44 +0200

    homework2

---
+++ homework2.py
@@ -0,0 +1,25 @@
+# Exercise 1
+my_word = input('Enter some word here: ')
+
+print(f'Word {my_word} has {len(my_word)} letters')
+
+# Exercise 2
+result = input('Hello! How old are you? ')
+try:
+    if int(result) <= 7:
+        print('Where are your parents?')
+    elif int(result) <= 16:
+        print('This is a movie for adults!')
+    elif int(result) >= 65:
+        print('Show your pension certificate!')
+    elif '7' in result:
+        print('You will be lucky today!')
+    else:
+        print('There are no more tickets!')
+except:
+    print('Please enter the integer')
+
+
+
+
+
