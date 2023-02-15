a234e7969ddb1fedd9c72176fb97ed3452974667
VasylynaI <vasilina.zaharko@gmail.com>
2023-02-15 18:21:10 +0200

    changes

--- homework6.py
+++ homework6.py
@@ -4,10 +4,8 @@
 # повертає результат. Якшо обидва аргументи відносяться до типу стрінг функція обʼєднує їх в один і повертає
 # В будь-якому іншому випадку - функція повертає кортеж з двох агрументів
 def my_function(arg1, arg2):
-    if is_bool_val(arg1) or is_bool_val(arg2):
-        return (arg1, arg2)
 
-    elif is_int(arg1) and is_int(arg2):
+    if is_int(arg1) and is_int(arg2):
         return arg1 * arg2
 
     elif is_str(arg1) and is_str(arg2):
@@ -16,14 +14,12 @@ def my_function(arg1, arg2):
     else:
         return (arg1, arg2)
 
-def is_bool_val(arg):
-    return isinstance(arg, bool)
-
-def is_int(arg):
-    return isinstance(arg, int)
-
 def is_str(arg):
     return isinstance(arg, str)
+
+def is_int(arg):
+    if type(arg) in (float, int):
+        return float, int
 
 # Exercise 2
 # Напишіть программу "Касир в кінотеватрі", яка попросіть користувача ввести свсвій вік (можно використати константу або функцію input(), на екран має бути виведено лише одне повідомлення, також подумайте над варіантами, коли введені невірні дані).
@@ -38,28 +34,33 @@ def take_user_age():
         return user_age
     except:
         print('Please enter the integer')
-
+        return take_user_age()
 
 def checking_user_age(age):
-    if age <= 0:
-        print('You have a strange age!')
+    if '7' in str(age) or age == 7:
+        print('You will be lucky today!')
     elif age <= 7:
         print('Where are your parents?')
     elif age <= 16:
         print('This is a movie for adults!')
     elif age >= 65:
         print('Show your pension certificate!')
-    elif set(str(age)).intersection('7'):
-        print('You will be lucky today!')
     else:
         print('There are no more tickets!')
 
-
-def game():
+def validation_user_age():
     while True:
         user_answer = take_user_age()
-        if user_answer:
+        if user_answer <= 0 or user_answer > 100:
+            print('You have a strange age!')
+        else:
             checking_user_age(user_answer)
+        break
+
+def game():
+    validation_user_age()
+
+game()
 
 
 
